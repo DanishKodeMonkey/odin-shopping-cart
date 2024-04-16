dec7f917984ad3e370d3ee062a6ab01dc1cacf0d
danishkodemonkey <humberjini@hotmail.com>
2024-04-16 17:06:41 +0200

    fix: add prefered theme check on mount of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import './App.css';
+import { CartProvider } from './CartContext';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { CartProvider } from './CartContext';
-import { useState } from 'react';
+
+import './App.css';
 
 function App() {
+    // check which theme to render as on load (thanks internet)
+    useEffect(() => {
+        if (
+            window.matchMedia &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+        ) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+    }, []);
     return (
         <CartProvider>
             <div className='app-container'>
